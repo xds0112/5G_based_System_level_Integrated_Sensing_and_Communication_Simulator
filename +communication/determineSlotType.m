7ffdcc9004d0d27c59d7ae220406467bca207afc
Dongsheng Xue <xuedongsheng@bupt.edu.cn>
2023-09-07 10:43:15 +0800

    Update determineSlotType.m

--- +communication/determineSlotType.m
+++ +communication/determineSlotType.m
@@ -2,7 +2,6 @@ function slotType = determineSlotType(tddPattern, slotIdx)
 %DETERMINESLOTTYPE 
 %   
     % Determine the current slot type
-    % symbols
     slotType = tddPattern(mod(slotIdx, numel(tddPattern)) + 1);
 
 end
