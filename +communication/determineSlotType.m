b4b60aa9ac56ecefac654a0b58a732f1d6da20b9
Dongsheng Xue <xuedongsheng@bupt.edu.cn>
2023-09-07 10:45:58 +0800

    Delete +communication/determineSlotType.m
    No longer need that

--- +communication/determineSlotType.m
+++
@@ -1,8 +0,0 @@
-function slotType = determineSlotType(tddPattern, slotIdx)
-%DETERMINESLOTTYPE 
-%   
-    % Determine the current slot type
-    slotType = tddPattern(mod(slotIdx, numel(tddPattern)) + 1);
-
-end
-
