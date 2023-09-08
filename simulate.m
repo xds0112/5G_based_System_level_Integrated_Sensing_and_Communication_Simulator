7b8281b21c2dbffb42f598557aaa8fbee0295539
Dongsheng Xue <xuedongsheng@bupt.edu.cn>
2023-09-08 15:21:37 +0800

    Update simulate.m

--- simulate.m
+++ simulate.m
@@ -7,7 +7,7 @@ function results = simulate(scenarioFunctionHandle, enableParallelSim)
 % Ministry of Education, BUPT.
 
     %% BUPT copyright    
-    printCopyRight
+    tools.printCopyright
 
     %% Initialize simulation parameters
     simuParams = parameters.simulationParameters;
@@ -20,22 +20,5 @@ function results = simulate(scenarioFunctionHandle, enableParallelSim)
     results = struct;
     results.communicationResults = comResults;
     results.sensingResults       = senResults;
-
-    %% Local functions
-    function printCopyRight
-
-        currentTime = datetime('now');
-    
-        fprintf('5G-based System-level Integrated Sensing and Communication Simulator\n')
-        fprintf('\n')
-        fprintf('Copyright (C) 2023 Beijing University of Posts and Telecommunications\n')
-        fprintf('All rights reserved.\n')
-        fprintf('\n')
-        fprintf('Authors: Dongsheng Xue, et.al\n')
-        fprintf('Key Laboratory of Universal Wireless Communications, Ministry of Education\n')
-        fprintf('Date: %s\n', char(currentTime))
-        fprintf('\n')
-
-    end
 
 end
